05da37d28adcd32b154142799ac90f818e753cea
warhand <wise_9@naver.com>
2019-01-16 14:03:02 +0900

    delete

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 	"time"
+	"fmt"
 )
 
 func main() {
@@ -15,4 +17,23 @@ func main() {
 
 
 	server.ListenAndServe(":8080", nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println("userID: ", r.Form["userID"])
+	fmt.Println("userPW: ", r.Form["userPW"])
+
+	// get from Chaincode
+	user_state := 0
+	
+	if user_state==1 {
+		t, _ := template.ParseFiles("user_menu.html")
+	}
+	else {
+		t, _ := template.ParseFiles("admin_menu.html")
+	}
+	t.Execute(w, nil)
+*/
 }
